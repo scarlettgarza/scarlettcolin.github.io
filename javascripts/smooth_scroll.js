c01863709b52840e29439c27eb579cbec95330a3
Scarlett Garza <scarlettgarza@icalialabs.com>
2017-10-16 11:13:07 -0500

    Adds build files

---
+++ javascripts/smooth_scroll.js
@@ -0,0 +1,8 @@
+$(function(){
+  $('a[href*=#]').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+    return false;
+  });
+});
